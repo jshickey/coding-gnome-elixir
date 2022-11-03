caad406295008d98afa184828f86fdaa785b100e
Scott Hickey <jshickey@protonmail.me>
2022-11-02 19:52:06 -0500

    moved word list into a module attribute

--- hangman/dictionary/lib/dictionary.ex
+++ hangman/dictionary/lib/dictionary.ex
@@ -1,9 +1,11 @@
 defmodule Dictionary do
-  def word_list do
-    "assets/words.txt"
-    |> File.read!()
-    |> String.split()
-  end
 
-  def random_word(), do: Enum.random(word_list())
+  @word_list "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/, trim: true)
+
+  def random_word() do
+    @word_list
+    |> Enum.random()
+  end
 end
